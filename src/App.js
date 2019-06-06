eddc5be6c29852a059c5df9f3186aa1fbee99f92
Begoja <49468478+Begoja@users.noreply.github.com>
2019-06-06 21:04:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://shrouded-atoll-61897.herokuapp.com/', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
