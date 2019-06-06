c9e417d1927d9b4fa4da5a02489848016ab46878
Begoja <49468478+Begoja@users.noreply.github.com>
2019-06-06 21:08:26 +0200

    Update Signin.js

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignin = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://shrouded-atoll-61897.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
