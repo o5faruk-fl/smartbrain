9813e645c15f5e52d0cb94475da500e4174170a8
Begoja <49468478+Begoja@users.noreply.github.com>
2019-06-06 21:06:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://shrouded-atoll-61897.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
